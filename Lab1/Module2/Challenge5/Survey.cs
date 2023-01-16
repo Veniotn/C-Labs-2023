d08c1bf090d563b50281853bd96db43896e3936e
Veniotn <w0404614@nscc.ca>
2023-01-16 03:09:10 -0400

    Module 2 commit, challenge 5 complete

---
+++ Lab1/Module2/Challenge5/Survey.cs
@@ -0,0 +1,50 @@
+﻿namespace Survey;
+
+public class Survey
+{
+    public string name;
+    private int age;
+    private string month;
+    private string country;
+    private double testResult;
+
+    public string Name
+    {
+        get => name;
+        set => name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Age
+    {
+        get => age;
+        set => age = value;
+    }
+
+    public string Month
+    {
+        get => month;
+        set => month = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Country
+    {
+        get => country;
+        set => country = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public double TestResult
+    {
+        get => testResult;
+        set => testResult = value;
+    }
+    
+
+    public Survey(string name, int age, string month, string country, double testResult)
+    {
+        this.name = name;
+        this.age = age;
+        this.month = month;
+        this.country = country;
+        this.testResult = testResult;
+    }
+}
---
+++ Lab1/Module2/Challenge5/Util.cs
@@ -0,0 +1,10 @@
+﻿namespace Util;
+
+public class Console
+{
+    public static string Ask(string question)
+    {
+       System.Console.WriteLine(question);
+        return System.Console.ReadLine();
+    }
+}
