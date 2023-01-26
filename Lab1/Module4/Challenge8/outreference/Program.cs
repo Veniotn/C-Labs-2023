17b3d9983b10180a73247dadd22330cb9caa0294
Veniotn <w0404614@nscc.ca>
2023-01-25 22:56:32 -0400

    Outreference challenge, Module 4 section 2

---
+++ Lab1/Module4/Challenge8/outreference/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+//outreference challenge 
+
+namespace outreference
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var test = "1.5";
+            var outDouble = 0.0;
+            Console.WriteLine("Result: {0}", parseDouble(test, out outDouble));
+            Console.WriteLine("Test type is {0}", outDouble.GetType());
+            Console.WriteLine("Test value is {0}", outDouble);
+            test = "Ronan";
+            outDouble = 0.0;
+            Console.WriteLine("Result: {0}", parseDouble(test,out  outDouble));
+            Console.WriteLine("Test type is {0}", outDouble.GetType());
+            Console.WriteLine("Test value is {0}", outDouble);
+        }
+        
+        //
+        // static void Five(out int a)
+        // {
+        //     a = 5;
+        //     Console.WriteLine(a);
+        // }
+
+        static bool parseDouble(string toParse, out double result)
+        {
+            result = 0.0;
+            try
+            {
+                result = Convert.ToDouble(toParse);
+                return true;                                        
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
