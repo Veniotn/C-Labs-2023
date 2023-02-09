9a7c7551c870d51367ecd6aeae2300babc9aca34
Veniotn <w0404614@nscc.ca>
2023-02-09 13:07:39 -0400

    Module1 commit

---
+++ Lab2/Module1/Calculator/SalaryCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Calculator;
+
+
+public class SalaryCalculator
+{
+    const int hoursInYear = 2080;
+    
+    public decimal GetAnnualSalary(decimal hourlyWage)
+    {
+        decimal result = hourlyWage * hoursInYear;
+        if (result <= 0)
+        {
+            throw new InvalidOperationException("Hourly wage must be greater than zero.");
+        }
+
+        return result;
+
+    }
+
+
+    public decimal getHourlyWage(int salary)
+    {
+        decimal result = salary / hoursInYear;
+        if (result <= 0)
+        {
+            throw new InvalidOperationException("Yearly salary must be greater than zero.");
+        }
+
+        return result;
+    }
+
+    public TaxData TaxWithheld(double weeklySalary, int numDependants)
+    {
+        if (numDependants < 0)
+        {
+            throw new InvalidOperationException("Number dependents cannot be negative.");
+        }
+
+        if (weeklySalary <= 0)
+        {
+            throw new InvalidOperationException("Weekly salary must be greater than zero.");
+        }
+        
+        const double provincialTaxRate = .06;
+        const double federalTaxRate = .25;
+        const double dependantRate = .02;
+        
+        double oneDependant = weeklySalary * dependantRate;
+        double dependantDeduction = oneDependant * numDependants;
+        
+        double provincialTaxWithheld = weeklySalary * provincialTaxRate;
+        double federalTaxWithHeld = weeklySalary * federalTaxRate;
+        double totalWithHeld = (provincialTaxWithheld + federalTaxWithHeld) - dependantDeduction;
+        double totalTakeHome = weeklySalary - totalWithHeld;
+        
+        return new TaxData(provincialTaxWithheld,federalTaxWithHeld,dependantDeduction, totalWithHeld, totalTakeHome);
+    }
+
+
+
+}
---
+++ Lab2/Module1/Calculator/TaxData.cs
@@ -0,0 +1,51 @@
+﻿namespace Calculator;
+
+public class TaxData
+{
+    private double provincialTaxWithheld;
+    private double federalTaxWithheld;
+    private double dependentDeduction;
+    private double totalWithheld;
+    private double totalTakeHome;
+
+
+    public TaxData(double provincialTaxWithheld, double federalTaxWithheld, double dependentDeduction, double totalWithheld, double totalTakeHome)
+    {
+        this.provincialTaxWithheld = provincialTaxWithheld;
+        this.federalTaxWithheld = federalTaxWithheld;
+        this.dependentDeduction = dependentDeduction;
+        this.totalWithheld = totalWithheld;
+        this.totalTakeHome = totalTakeHome;
+    }
+
+
+    public double ProvincialTaxWithheld
+    {
+        get => provincialTaxWithheld;
+        set => provincialTaxWithheld = value;
+    }
+
+    public double FederalTaxWithheld
+    {
+        get => federalTaxWithheld;
+        set => federalTaxWithheld = value;
+    }
+
+    public double DependentDeduction
+    {
+        get => dependentDeduction;
+        set => dependentDeduction = value;
+    }
+
+    public double TotalWithheld
+    {
+        get => totalWithheld;
+        set => totalWithheld = value;
+    }
+
+    public double TotalTakeHome
+    {
+        get => totalTakeHome;
+        set => totalTakeHome = value;
+    }
+}
