0b3b2429d27dad5847f8ce433b58ce2459ece14d
Veniotn <w0404614@nscc.ca>
2023-02-09 13:02:00 -0400

    Main program file. module2 section 2

---
+++ Lab2/Module2/Module2/Module2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+
+namespace Module2
+{
+    
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            const int hours = 55, wage = 70;
+            List<Employee> employees = Util.GetEmployees();
+            List<Contractor> contractors = Util.getContractors();
+
+            foreach (var e in employees)
+            {
+                e.CalculateWeeklySalary(hours, wage);
+            }
+            
+            foreach (var c in contractors)
+            {
+                c.CalculateWeeklySalary(hours, wage);
+            }
+        }
+
+
+    }
+}
